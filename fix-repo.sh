c49f9ef871b0c6c3b08f68f1d6805e05c58475ef
adnan1175 <138349081+adnan1175@users.noreply.github.com>
2023-07-02 20:16:36 +0600

    Update fix-repo.sh

--- fix-repo.sh
+++ fix-repo.sh
@@ -4,4 +4,4 @@ mkdir -p $PREFIX/etc/apt/sources.list.d
 echo "deb [trusted=yes arch=all] https://ivam3.github.io/termux-packages stable extras" >$PREFIX/etc/apt/sources.list.d/ivam3-termux-packages.list
 apt update -y
 pkg update -y
-
+apt install x11-repo tur-repo root-repo -y
