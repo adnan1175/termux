91e80803d1f310cb7d26384e10a29076272a6981
adnan1175 <138349081+adnan1175@users.noreply.github.com>
2023-07-03 09:42:23 +0600

    Add files via upload

---
+++ openbox.sh
@@ -0,0 +1,4 @@
+apt install git -y
+git clone https://github.com/adnan1175/termux-desktop
+cd termux-desktop
+bash setup.sh
---
+++ xfce4.sh
@@ -0,0 +1,4 @@
+apt install git 
+git clone https://github.com/adnan1175/termux-desktop-xfce
+cd termux-desktop-xfce
+bash bootstrap.sh
